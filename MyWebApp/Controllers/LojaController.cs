acdad92015ddd221b6a7d0d8fd4f4dbefddc3065
Felipe dos Santos Carvalho <felipe_fsc@hotmail.com>
2018-06-24 09:43:17 -0300

    componente simples

---
+++ MyWebApp/Controllers/LojaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
